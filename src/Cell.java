d793ac19b60920a488bb3e1a47b571d08cb7aac4
HK12390 <hanna.w.khan@gmail.com>
2021-09-05 18:44:00 -0700

    Assignment 3

---
+++ src/Cell.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Cell {
+
+    public int strength;
+    public int x;
+    public int y;
+    public int id;
+    private ArrayList<Cell> neighbors;
+
+    public Cell(int strength, int id, int x, int y){
+        this.strength = strength;
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+    public Cell(){
+        this( 0,0, 0,  0);
+    }
+
+    public static void main(String[] args) {
+        Cell newCell = new Cell(-1,2,3,4);
+        newCell.setStrength(newCell.strength);
+        newCell.setID(newCell.id);
+        newCell.setX(newCell.x);
+        newCell.setY(newCell.y);
+
+        System.out.println(newCell.strength);
+        System.out.println(newCell.id);
+        System.out.println(newCell.x);
+        System.out.println(newCell.y);
+    }
+
+
+    public void setStrength(int strength) {
+        this.strength = Math.max(strength, 0);
+    }
+    public void setID (int id) {
+        this.id = Math.max(id, 0);
+    }
+    public void setX (int x) {
+        this.x = Math.max(x, 0);
+    }
+    public void setY (int y) {
+        this.y = Math.max(y, 0);
+    }
+
+    public void interactNeighbors (ArrayList<Cell> neighbors){
+
+    }
+}
